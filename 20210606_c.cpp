f7d79c56021e64a828bb7a033d6c79d2e8804ca2
Mten7271 <80489033+MamiKikuchi@users.noreply.github.com>
2021-06-16 15:48:49 +0900

    AtCorderの提出コード

--- 20210606_c.cpp
+++ 20210606_c.cpp
@@ -1,23 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+vector<vector<int>> G;
+vector<int> seen(2000,false);
+
+void  dfs(int v){
+    if(seen[v]) return;
+    //seen[v]を検知済みにする
+    seen[v] = true;
+
+    for(auto next_v : G[v]){
+      if(seen[next_v]) continue;//next_vの時探索済みなら飛ばす
+      dfs(next_v);
+    }
+
+
+}
+
 int main(){
-  int N,M,A[2000],B[2000],pair=0;
+  int N,M,a,b;
   cin >> N >> M;
 
+  G.resize(N);
 
-  for(int i=0;i<M;i++) cin >> A[i] >> B[i];
-  
-  for(int i=0;i<M;i++){
-      for(int j=0;j<M;j++){
-          if(B[i]==A[j] && A[i]!=B[j]) pair+=abs(A[i]-B[j]);
-          //cout << rout <<endl;
-      }
+  for(int i=0;i<M;i++) {
+    cin >> a >> b;
+    G[a-1].push_back(b-1);
   }
   
-pair+=N+M;
+  int ans=0;
+
+  for(int i=0; i<N; i++){
+    for(int j=0;j<N;j++) seen[j]=false;
+     dfs(i);
+     for(int j=0; j<N; j++) if(seen[j])ans++;
+  }
+  
  
-  cout << pair << endl;
+  cout << ans << endl;
   
   return 0;
 }
