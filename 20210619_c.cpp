054153a0f1ef9280418938bc8a1bf17754559629
Mten7271 <80489033+MamiKikuchi@users.noreply.github.com>
2021-06-22 14:24:56 +0900

    AtCorderの提出コード

--- 20210619_c.cpp
+++ 20210619_c.cpp
@@ -1,43 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> G;
-vector<int> seen(1000000000,false);
-
-void  dfs(int v){
-    if(seen[v]) return;
-    //seen[v]を検知済みにする
-    seen[v] = true;
-
-    for(auto next_v : G[v]){
-      if(seen[next_v]) continue;//next_vの時探索済みなら飛ばす
-      dfs(next_v);
-    }
-
-
-}
 
 int main(){
   int N,a;
+  vector<int> A;
   cin >> N;
 
-  G.resize(N);
+  A.resize(N);
 
-  for(int i=0;i<N;i++) {
-    cin >> a;
-    G[i].push_back(a-1);
-  }
-  
-  int ans=0;
+  for(int i=0; i<N; i++) cin >> A[i];
 
-  for(int i=0; i<N; i++){
-    for(int j=0;j<N;j++) seen[j]=false;
-     dfs(i);
-     for(int j=0; j<N; j++) if(seen[j])ans++;
-  }
-  
+  map<int,int> cnt;
+  long long ans = 0;
+
+  for(int j=0;j<N;j++){
+    ans+=j-cnt[A[j]];
+   //cout << cnt[A[j]] << ans << endl;
+    //A[i]の値を1ずつ
+    cnt[A[j]]++;
  
+  }
+
   cout << ans << endl;
-  
-  return 0;
+
 }
