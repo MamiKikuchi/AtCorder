5797a2c1d47c6e5608a0b27e41b8c34e98871ace
Mten7271 <80489033+MamiKikuchi@users.noreply.github.com>
2021-07-03 00:13:15 +0900

    AtCorderの提出コード

--- 20161023_a.cpp
+++ 20161023_a.cpp
@@ -8,6 +8,10 @@ int main(){
     int num = 0;
 
     for(int i=0;i<int(s.size());i++){
-        if(s.at(i) == 'C')
+        if(s.at(i) == 'C') num = 1;
+        else if( num ==1 && s.at(i) == 'F') num = 2;
     }
+
+    if(num == 2) cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
