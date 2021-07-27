92fada34f27d23a176c5b3f1878749bdbd259aad
Mten7271 <80489033+MamiKikuchi@users.noreply.github.com>
2021-07-27 17:19:13 +0900

    AtCorderの提出コード

--- 20210724_c.cpp
+++ 20210724_c.cpp
@@ -1,9 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string s;
+int term_chk = 0;
+vector<int> seen;
+char term[] = {'c','h','o','k','u','d','a','i'};
+
+void df(int i){
+    if(seen[i]) return;
+    if(s.at(i) == term[term_chk]){
+        seen[i] = true;
+       // cout << i << s.at(i) << term[term_chk] << seen[i] << endl;
+        term_chk++;
+        
+    }
+
+     for(int j=i+1;j<int(s.size());j++){
+        if(seen[j]) continue;
+        df(j);
+    }
+
+}
+
 int main(){
-    string s;
+
     cin >> s;
 
     long long MOD = 1000000007;
+
+    seen.resize(int(s.size()));
+
+    int ans =0;
+    
+
+    for(int i=0;i<int(s.size());i++){
+        term_chk = 0;
+        int count = 0;
+        for(int j=0;j<int(s.size());j++) seen[j] = false;
+        df(i);
+        for(int j=0;j<int(s.size());j++) if(seen[j]) count++;
+        if(count == 8) ans++;
+    }
+
+    cout << ans%MOD << endl;
 }
---
+++ 20210725_a.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    vector<int> a(N, 0);
+    vector<int> b(N, 0);
+    for (int i = 0; i < K; i++)
+    {
+        char c;
+        int k;
+        cin >> c >> k;
+        k--;
+        b[k] = 1;
+        for (int j = 0; j < N; j++)
+        {
+            if (c == 'L' && k <= j)
+                a[j]++;
+            if (c == 'R' && j <= k)
+                a[j]++;
+        }
+    }
+    const ll MOD = 998244353;
+    ll ans = 1;
+    for (int i = 0; i < N; i++)
+        if (!b[i])
+            ans = ans * a[i] % MOD;
+    cout << ans << endl;
+    return 0;
+}
