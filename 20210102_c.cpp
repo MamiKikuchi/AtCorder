0a91943d2e5b83d13b48ef511a8cac1e51abcc09
Mten7271 <80489033+MamiKikuchi@users.noreply.github.com>
2022-05-04 22:16:17 +0900

    AtCorderの提出コード

---
+++ 20210102_c.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+#define REP(i,n) for(int i=0; i<(n); i++)
+#define REP2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+#define PRINT(n) cout << n << endl;
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n;
+    cin >> n;
+    vs S(n);
+    map<string, int> count{};
+    REP(i, n){
+        cin >> S[i];
+        count[S[i]]++;
+    }
+
+    string ans = "satisfiable";
+
+    REP(i, n){
+        if(count.count(S[i]) > 1) ans = S[i];
+        string tmp = "!" + S[i];
+        if(count.count(tmp)+1 > 1) ans = S[i];
+    }
+    PRINT(ans)
+    return 0;
+}
