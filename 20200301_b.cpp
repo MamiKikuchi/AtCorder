4620207fa24a5937634df452c6478b87165d7e44
Mten7271 <80489033+MamiKikuchi@users.noreply.github.com>
2022-05-24 01:03:50 +0900

    AtCoderの提出コード

--- 20200301_b.cpp
+++ 20200301_b.cpp
@@ -26,6 +26,25 @@ int main()
     vi b(n);
     REP(i, n) cin >> b[i];
     int ans[3][3] = {0, 0};
-    REP(i, n)
+    REP(i, n) REP(j, 3) REP(k, 3) if(b[i]==A[j][k]) ans[j][k]=1;
+    
+    if(ans[0][0]==1 && ans[1][1]==1 && ans[2][2]==1){
+        PRINT("Yes")
+        return 0;
+    }else if(ans[0][2]==1 && ans[1][1]==1 && ans[2][0]==1){
+        PRINT("Yes")
+        return 0;
+    }else{
+        REP(i, 3){
+            if(ans[i][0]==1 && ans[i][1]==1 && ans[i][2]==1){
+                PRINT("Yes")
+                return 0;
+            }else if(ans[0][i]==1 && ans[1][i]==1 && ans[2][i]==1){
+                PRINT("Yes")
+                return 0;
+            }
+        }
+    }
+    PRINT("No")
     return 0;
 }
