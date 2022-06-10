87fc78d405cf3552724fcd2ca78274b7e9b62529
Mten7271 <mami.kikuchi2414@gmail.com>
2022-06-11 01:11:13 +0900

    AtCoderの提出コード

---
+++ 20220815_b.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+#define REP(i,n) for(int i=0; i<(n); i++)
+#define REP2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+#define PRINT(n) cout << n << endl;
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n;
+    cin >> n;
+    vi L(n);
+    REP(i, n) cin >> L[i];
+    int count = 0;
+    sort(ALL(L));
+    REP(i, n){
+        REP2(j, i+1, n){
+            REP2(k, j+1, n){
+                if(L[k]<L[i]+L[j] && L[k]!=L[i] && L[k]!=L[j] && L[i]!=L[j]){
+                    count++;
+                }
+            }
+        }
+    }
+    PRINT(count)
+    return 0;
+}
