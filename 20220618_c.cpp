7f8527b8a761a5227c357ce99f34b4eade324d65
Mten7271 <mami.kikuchi2414@gmail.com>
2022-06-19 08:31:07 +0900

    AtCoderの提出コード

---
+++ 20220618_c.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+#define REP(i,n) for(int i=0; i<(n); i++)
+#define REP2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+#define PRINT(n) cout << n << endl;
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    vi h(3), w(3);
+    REP(i, 3) cin >> h[i];
+    REP(i, 3) cin >> w[i];
+
+    REP(i, 3){
+        
+    }
+    return 0;
+}
