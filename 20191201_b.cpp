b672d4561700fc128dc248886c1aabe74656890c
Mten7271 <mami.kikuchi2414@gmail.com>
2022-06-21 02:06:11 +0900

    AtCoderの提出コード

---
+++ 20191201_b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+#define REP(i,n) for(int i=0; i<(n); i++)
+#define REP2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+#define PRINT(n) cout << n << endl;
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+const long long INF = numeric_limits<long long>::max();
+int main()
+{
+    int n;
+    cin >> n;
+
+    int X = (double)n / 1.08;
+    
+    if(X*1.08 >= n) PRINT(X)
+    else if(X*1.08<n && int((X+1)*1.08)==n) PRINT(X+1)
+    else PRINT(":(")
+
+    return 0;
+}
